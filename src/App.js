7bf72e21610b3d7a985ceae3696b688e136b3b78
carly <carly@noukod.com>
2019-03-26 16:49:54 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import CatComponent from './CatComponent.js'
-import { FordQuoteComponent } from './FordQuoteComponent'
+import FordQuoteComponent from './FordQuoteComponent'
 import MouseComponent from '../MouseComponent'
 
 class App extends Component {
